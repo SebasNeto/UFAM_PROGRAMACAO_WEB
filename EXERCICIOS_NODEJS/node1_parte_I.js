6ce9b4dc3fb3bbc028765dccc2b2acba86a3699a
sebastiao <netoamt.sebastiao1@gmail.com>
2024-06-20 22:34:33 -0400

    commit EXERCICIOS_NODEJS

---
+++ EXERCICIOS_NODEJS/node1_parte_I.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = process.argv[2];
+if (!directoryPath) {
+  console.error('esqueceu do argumento (diretório).');
+  process.exit(1);
+}
+
+const server = http.createServer((req, res) => {
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('Houve erro na leitura do diretório');
+      return;
+    }
+    
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<html><body><ul>');
+    
+    files.forEach(file => {
+      res.write(`<li>${file}</li>`);
+    });
+    
+    res.write('</ul></body></html>');
+    res.end();
+  });
+});
+
+const PORT = 3333;
+server.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
