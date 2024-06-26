b669d15fa680926f246e231a418856c9822d4838
sebastiao <netoamt.sebastiao1@gmail.com>
2024-06-26 12:01:58 -0400

    commit de atualizações game snake

--- Tp1_Jogo_Snake/Js/game.js
+++ Tp1_Jogo_Snake/Js/game.js
@@ -1,11 +1,11 @@
-(function () { // (IIFE) - func definida e executada imediatamente
-  let FPS = 10; // velocidade do jogo
-  const SIZE = 40; // tamanho do tabuleiro 40x40
+(function () {
+  let FPS = 10; 
+  const SIZE = 40; 
 
-  // armazenam instâncias do jogo
+  // armazenam instâncias do jogo.
   let board, snake, comecomeSnake, score, intervaloId, descanso = false;
 
-  // inicializa o jogo criando o tabuleiro e a cobra
+  // função que inicializa o jogo criando o tabuleiro e a cobra.
   function init() {
       if (board) {
           document.body.removeChild(board.element);
@@ -16,20 +16,20 @@
       score = 0;
       atualisaScore();
       generatecomecomeSnake();
-      document.getElementById('game-over').style.display = 'none'; // Esconde a mensagem de Game Over
+      document.getElementById('game-over').style.display = 'none';
       if (intervaloId) {
           clearInterval(intervaloId);
       }
-      intervaloId = setInterval(run, 1000 / FPS); // Inicia o loop do jogo chamando a função run a cada / FPS
-      document.getElementById('musica-fundo').play(); //inicia som de fundo
+      intervaloId = setInterval(run, 1000 / FPS); 
+      document.getElementById('musica-fundo').play();
   }
 
-  // atualiza o score no HTML
+  // função que atualiza o score no HTML.
   function atualisaScore() {
       document.getElementById('score').innerText = score.toString().padStart(5, '0');
   }
 
-  // evento do teclado - captura as teclas de seta pressionadas pelo usuário
+  // evento do teclado - captura as teclas de seta pressionadas pelo usuário.
   window.addEventListener("keydown", (e) => {
       switch (e.key) {
           case "ArrowUp": // seta para cima
@@ -55,45 +55,48 @@
       }
   });
 
+  //função que define a pausa do jogo.
   function horaDaPausa() {
       descanso = !descanso;
       if (descanso) {
           clearInterval(intervaloId);
-          document.getElementById('musica-fundo').pause(); //pausa som de fundo
+          document.getElementById('musica-fundo').pause();
       } else {
           intervaloId = setInterval(run, 1000 / FPS);
-          document.getElementById('musica-fundo').play(); //retoma som de fundo
+          document.getElementById('musica-fundo').play();
       }
   }
 
+  //função que cria o tabuleiro do jogo.
   class Board {
-      constructor(size) { // cria uma tabela HTML com linhas e colunas
+      constructor(size) {
           this.element = document.createElement("table");
           this.element.setAttribute("id", "board");
-          this.color = "#ccc";
-          document.body.appendChild(this.element); // adiciona a tabela ao corpo do documento HTML
+          this.color = "transparent";
+          document.body.appendChild(this.element);
           for (let i = 0; i < size; i++) {
-              const row = document.createElement("tr"); // linhas
+              const row = document.createElement("tr"); 
               this.element.appendChild(row);
               for (let j = 0; j < size; j++) {
-                  const field = document.createElement("td"); // elementos internos
+                  const field = document.createElement("td");
                   row.appendChild(field);
               }
           }
       }
   }
 
+  //função que representa a Snake no jogo.
   class Snake {
-      constructor(body) { // inicializa a cobra com um corpo
-          this.body = body; // posição atual
+      constructor(body) { 
+          this.body = body; 
           this.color = "#222";
-          this.direction = 1; // 0 para cima, 1 para direita, 2 para baixo, 3 para esquerda
-          this.body.forEach(field => this.corCelula(field, this.color)); // pinta cada segmento
+          this.direction = 1; 
+          this.body.forEach(field => this.corCelula(field, this.color)); 
       }
-      // move a cobra na direção atual
+      // move a cobra na direção atual.
       walk() {
           const head = this.body[this.body.length - 1];
-          let newHead; // posição da cabeça
+          let newHead; 
           switch (this.direction) {
               case 0:
                   newHead = [head[0] - 1, head[1]];
@@ -113,7 +116,7 @@
 
           if (this.cuidadoSnake(newHead))
               return perdeuSnake();
-          this.body.push(newHead); // adiciona newHead ao final do corpo da cobra
+          this.body.push(newHead); 
 
           if (newHead[0] == comecomeSnake[0] && newHead[1] == comecomeSnake[1]) {
               score += comecomeSnake[2];
@@ -124,30 +127,32 @@
               const caldaVelha = this.body.shift();
               this.corCelula(caldaVelha, board.color);
           }
-          this.corCelula(newHead, this.color); // pinta a nova cabeça
+          this.corCelula(newHead, this.color); 
       }
-      // atualiza a direção da cobra para a direção fornecida, evitando 180 graus
+      // atualiza a direção da cobra para a direção fornecida, evitando 180 graus.
       changeDirection(direction) {
           if (Math.abs(this.direction - direction) !== 2) {
               this.direction = direction;
           }
       }
 
-      // célula específica, caminho da cobra
+      // célula específica, caminho da cobra.
       corCelula(posicao, color) {
           document.querySelector(`#board tr:nth-child(${posicao[0] + 1}) td:nth-child(${posicao[1] + 1})`).style.backgroundColor = color;
       }
 
-      // verificando colisão com corpo e parede
+      // verificando colisão com corpo e parede.
       cuidadoSnake([row, bateuSnake]) {
           return row < 0 || bateuSnake < 0 || row >= SIZE || bateuSnake >= SIZE || this.body.some(segment => segment[0] === row && segment[1] === bateuSnake);
       }
 
+      //limpa a cobra do tabuleiro.
       clearSnake() {
           this.body.forEach(segment => this.corCelula(segment, board.color));
       }
   }
 
+  //função que gera comida para a Snake em posição aleatória.
   function generatecomecomeSnake() {
       let row, col;
       do {
@@ -159,17 +164,18 @@
       document.querySelector(`#board tr:nth-child(${row + 1}) td:nth-child(${col + 1})`).style.backgroundColor = type === 1 ? 'black' : 'red';
   }
 
+  //função que executa ações no caso de gamer over. 
   function perdeuSnake() {
       snake.clearSnake();
       clearInterval(intervaloId);
       intervaloId = null;
       document.getElementById('musica-fundo').pause();
       document.getElementById('over-game').play();
-      document.getElementById('game-over').style.display = 'block'; // Mostra a mensagem de Game Over
+      document.getElementById('game-over').style.display = 'block';
       document.getElementById('over-game').stop();
   }
 
-  // posição na direção atual
+  //função que controla o loop principal do jogo.
   function run() {
       if (!descanso) {
           snake.walk();
@@ -179,6 +185,6 @@
       }
   }
 
-  // inicia o jogo da cobra
+  //inicia o jogo.
   init();
 })();
